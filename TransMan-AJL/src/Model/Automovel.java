1ebfc6cee822ab01dece5ffacfad0c4efb5c15a4
Luis Mabjaia <luisgilmabjaia@gmail.com>
2018-09-02 20:58:26 +0300

    Reboque

--- TransMan-AJL/src/Model/Automovel.java
+++ TransMan-AJL/src/Model/Automovel.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 /**
  *
  * @author lipe
  */
-public class Automovel {
+public class Automovel extends Veiculo{
+      public Automovel(String matricula, String marca, String modelo, int ano, double kilometragem, String status, Documentacao doc) {
+        super(matricula, marca, modelo, ano, kilometragem, status, doc);
+    }
     
 }
