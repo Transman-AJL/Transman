dd0a690a655ebd8bcb88be85852ef54312f9efea
Luis Mabjaia <luisgilmabjaia@gmail.com>
2018-09-02 21:09:46 +0300

    Automovel

--- TransMan-AJL/src/Model/Automovel.java
+++ TransMan-AJL/src/Model/Automovel.java
@@ -7,6 +7,5 @@ package Model;
 public class Automovel extends Veiculo{
       public Automovel(String matricula, String marca, String modelo, int ano, double kilometragem, String status, Documentacao doc) {
         super(matricula, marca, modelo, ano, kilometragem, status, doc);
-    }
-    
+      }  
 }
