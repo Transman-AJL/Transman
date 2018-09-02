29e4f0a303f60410938b327d8aa0e8cbae23b9c6
Luis Mabjaia <luisgilmabjaia@gmail.com>
2018-09-02 20:39:10 +0300

    Bus

--- TransMan-AJL/src/Model/Documentacao.java
+++ TransMan-AJL/src/Model/Documentacao.java
@@ -1,14 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
+import java.util.Date;
 
 /**
  *
  * @author lipe
  */
-public class Documentacao {
+public class Documentacao { 
+    //Seguro
+    Date EmissaoSeg;
+    Date FimSeg;
+    //Inspensao
+    Date EmissaoIns;
+    Date FimIns;
+    //Manifesto
+    Date EmissaoMan;
+    Date FimMan;
+    //Radio
+    Date EmissaoRad;
+    Date FimRad;
+    //Manutencao
+    Date EmissaoMant;
+    Date FimMant;
+
+
+    
+    public Date getEmissaoSeg() {
+        return EmissaoSeg;
+    }
+
+    public void setEmissaoSeg(Date EmissaoSeg) {
+        this.EmissaoSeg = EmissaoSeg;
+    }
+
+    public Date getFimSeg() {
+        return FimSeg;
+    }
+
+    public void setFimSeg(Date FimSeg) {
+        this.FimSeg = FimSeg;
+    }
+
+    public Date getEmissaoIns() {
+        return EmissaoIns;
+    }
+
+    public void setEmissaoIns(Date EmissaoIns) {
+        this.EmissaoIns = EmissaoIns;
+    }
+
+    public Date getFimIns() {
+        return FimIns;
+    }
+
+    public void setFimIns(Date FimIns) {
+        this.FimIns = FimIns;
+    }
+
+    public Date getEmissaoMan() {
+        return EmissaoMan;
+    }
+
+    public void setEmissaoMan(Date EmissaoMan) {
+        this.EmissaoMan = EmissaoMan;
+    }
+
+    public Date getFimMan() {
+        return FimMan;
+    }
+
+    public void setFimMan(Date FimMan) {
+        this.FimMan = FimMan;
+    }
+
+    public Date getEmissaoRad() {
+        return EmissaoRad;
+    }
+
+    public void setEmissaoRad(Date EmissaoRad) {
+        this.EmissaoRad = EmissaoRad;
+    }
+
+    public Date getFimRad() {
+        return FimRad;
+    }
+
+    public void setFimRad(Date FimRad) {
+        this.FimRad = FimRad;
+    }
+
+    public Date getEmissaoMant() {
+        return EmissaoMant;
+    }
+
+    public void setEmissaoMant(Date EmissaoMant) {
+        this.EmissaoMant = EmissaoMant;
+    }
+
+    public Date getFimMant() {
+        return FimMant;
+    }
+
+    public void setFimMant(Date FimMant) {
+        this.FimMant = FimMant;
+    }
     
 }
