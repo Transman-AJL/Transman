7becfb541b903671e620293408dcd9bd11793bc3
Luis Mabjaia <luisgilmabjaia@gmail.com>
2018-09-02 20:11:55 +0300

    Automovel

--- TransMan-AJL/src/Model/Cliente.java
+++ TransMan-AJL/src/Model/Cliente.java
@@ -10,5 +10,32 @@ package Model;
  * @author lipe
  */
 public class Cliente {
+    private String codigo;
+    private String nome;
+    private String nuit;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNuit() {
+        return nuit;
+    }
+
+    public void setNuit(String nuit) {
+        this.nuit = nuit;
+    }
     
 }
--- TransMan-AJL/src/Model/Motorista.java
+++ TransMan-AJL/src/Model/Motorista.java
@@ -5,10 +5,52 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author lipe
  */
-public class Motorista {
+public class Motorista extends Funcionario{
     
+    private String categoria;
+    private Date dataEmissao;
+    private Date dataFim;
+    private int numCarta;
+    
+    public Motorista(int codigo, String nome, Date DataNascimento, Date DataAdmissao, String funcao, int numBI, boolean status) {
+        super(codigo, nome, DataNascimento, DataAdmissao, funcao, numBI, status);
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Date getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDataEmissao(Date dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public int getNumCarta() {
+        return numCarta;
+    }
+
+    public void setNumCarta(int numCarta) {
+        this.numCarta = numCarta;
+    }
 }
