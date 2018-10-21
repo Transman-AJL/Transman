0f5ed63e1fbcdef7f70d063bb9ca9e6f3fef5652
Luis Mabjaia <luisgilmabjaia@gmail.com>
2018-10-21 21:04:57 +0300

    Veiculo

--- TransMan-AJL/src/Model/Veiculo.java
+++ TransMan-AJL/src/Model/Veiculo.java
@@ -11,10 +11,16 @@ import javax.persistence.Id;
  * @author Arley Bebe
  */
 @Entity
+/**
+ * Criação da classe Veiculo
+ */
 public class Veiculo {
     
     @Id
     @GeneratedValue
+    /**
+     * criando variáveies da classe Veículo
+     */
     private int id;
     private String matricula;
     private String marca;
@@ -29,6 +35,14 @@ public class Veiculo {
     private double volume;
     //private Documentacao doc;
 
+    /**
+     * criação do metodo construtor da classe Veículo
+     * @param matricula
+     * @param marca
+     * @param modelo
+     * @param ano
+     * @param kilometragem 
+     */
     public Veiculo(String matricula, String marca, String modelo, int ano, double kilometragem) {
         this.matricula = matricula;
         this.marca = marca;
@@ -57,7 +71,10 @@ public class Veiculo {
     public String getCategoria() {
         return categoria;
     }
-
+    /**
+     * Metodo de alteração da categoria dos veículos
+     * @param categoria 
+     */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
@@ -65,7 +82,10 @@ public class Veiculo {
     public String getTipo() {
         return tipo;
     }
-
+    /**
+     * Metodo de alteração do tipo de veículo
+     * @param tipo 
+     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
@@ -73,7 +93,10 @@ public class Veiculo {
     public double getPeso() {
         return peso;
     }
-
+    /**
+     * Metodo de alteração do peso dos veículos
+     * @param peso 
+     */
     public void setPeso(double peso) {
         this.peso = peso;
     }
@@ -81,7 +104,10 @@ public class Veiculo {
     public int getLotacao() {
         return lotacao;
     }
-
+    /**
+     * Metodo de  alteração da lotação dos veículos
+     * @param lotacao 
+     */
     public void setLotacao(int lotacao) {
         this.lotacao = lotacao;
     }
@@ -89,11 +115,18 @@ public class Veiculo {
     public double getVolume() {
         return volume;
     }
-
+    /**
+     * Metodo de alteração do volume dos veículos do tipo Tanque
+     * @param volume 
+     */
     public void setVolume(double volume) {
         this.volume = volume;
     }
-
+    
+    /**
+     * Metodo de alteração da matricula dos veículos
+     * @param matricula 
+     */
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
@@ -102,6 +135,10 @@ public class Veiculo {
         return marca;
     }
 
+    /**
+     * Metodo de alteração da marca dos veiculos
+     * @param marca 
+     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
@@ -110,10 +147,18 @@ public class Veiculo {
         return modelo;
     }
 
+    /**
+     * Metodo de lalteração do modelo dos veículos
+     * @param modelo 
+     */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
+    /**
+     * Metodo de alteração do ano de fabrico dos veículos
+     * @return 
+     */
     public int getAno() {
         return ano;
     }
@@ -122,10 +167,17 @@ public class Veiculo {
         this.ano = ano;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public double getKilometragem() {
         return kilometragem;
     }
-
+    /**
+     * Metodo de alteração da kilometragem inicial do veículo na sua aquisição dos veículos
+     * @param kilometragem 
+     */
     public void setKilometragem(double kilometragem) {
         this.kilometragem = kilometragem;
     }
@@ -134,6 +186,10 @@ public class Veiculo {
         return status;
     }
 
+    /**
+     * Metodo de alteração do estado dos veículos 
+     * @param status 
+     */
     public void setStatus(String status) {
         this.status = status;
     }
